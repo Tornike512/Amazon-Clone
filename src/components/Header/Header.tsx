4f82a6b211a3c7d9ec19bd25629ffc40e6dba961
Tornike512 <Dht5152@gmail.com>
2023-12-28 23:40:27 +0400

    style: add nav-bar style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,20 @@
-import "./Header.scss";
+import navIcon from "@src/assets/nav-icon.png";
 
 export function Header() {
-  return <header className="header">header</header>;
+  return (
+    <header>
+      <div className="header-input"></div>
+      <nav className="nav-bar">
+        <a href="#">
+          <img src={navIcon} alt="nav icon" /> All
+        </a>
+
+        <a href="#">Today's Deals</a>
+        <a href="#">Registry</a>
+        <a href="#">Customer Service</a>
+        <a href="#">Gift Cards</a>
+        <a href="#">Sell</a>
+      </nav>
+    </header>
+  );
 }
