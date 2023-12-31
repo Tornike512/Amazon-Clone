b4231c599812105a2488ef5e9a12cf5fb8c624a3
Tornike512 <Dht5152@gmail.com>
2023-12-31 13:04:28 +0400

    style:add amazon logo, add location

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,28 @@
 import navIcon from "@src/assets/nav-icon.png";
+import amazonLogo from "@src/assets/amazon-logo.png";
+import locationLogo from "@src/assets/location-logo.png";
 
 export function Header() {
   return (
     <header>
-      <div className="header-input"></div>
+      <div className="header-input">
+        <img className="amazon-logo" src={amazonLogo} alt="Amazon logo" />
+        <div className="deliver-spacing">
+          <img
+            className="location-logo"
+            src={locationLogo}
+            alt="Location logo"
+          />
+          <button className="deliver">
+            <div>
+              <h5>Deliver to</h5>
+              <h4>
+                <b>United Kingdom</b>
+              </h4>
+            </div>
+          </button>
+        </div>
+      </div>
       <nav className="nav-bar">
         <a href="#">
           <img src={navIcon} alt="nav icon" /> All
