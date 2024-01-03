bb4a61d6777a71ebb5c959b5c67d16cfc1e8e8c5
Tornike512 <Dht5152@gmail.com>
2024-01-03 14:26:47 +0400

    Add: Your Lists and Your Account bar

--- src/components/SignInModal/SignInModal.tsx
+++ src/components/SignInModal/SignInModal.tsx
@@ -10,6 +10,16 @@ export function SignInModal() {
         <p className="sign-in-bar-register">
           New customer? <a href="#"> Start here</a>
         </p>
+        <div className="lists-account">
+          <span className="lists">
+            <span>Your Lists</span>
+            <a href="#">Create a List</a>
+          </span>
+          <span className="account">
+            <span>Your Account</span>
+            <a href="#">Account</a>
+          </span>
+        </div>
       </div>
       <img
         className="sign-in-triangle"
