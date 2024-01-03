b0aa47b3588f3b1971becb44cbd2bce77fd720aa
Tornike512 <Dht5152@gmail.com>
2024-01-03 18:36:26 +0400

    Add: Amazon Logo on Sign In Page

--- src/views/SignInPage/SignInPage.tsx
+++ src/views/SignInPage/SignInPage.tsx
@@ -1,8 +1,10 @@
+import "@src/views/SignInPage/SignInPage.scss";
+
 import amazonLogoBlack from "@src/assets/amazon-logo-black.png";
 
 export function SignInPage() {
   return (
-    <div>
+    <div className="sign-in-page">
       <img src={amazonLogoBlack} alt="Black Amazon Logo" />
     </div>
   );
