46fe5a31656bad352bb16528024ba3139e8bdb6b
Tornike512 <Dht5152@gmail.com>
2024-01-06 17:48:26 +0400

    Add: LocaleProvider For Language Change

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,12 +1,15 @@
 import { PropsWithChildren } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalProvider } from "./GlobalProvider/GlobalProvider";
+import { LocaleProvider } from "./LocaleProvider";
 import "@src/sass/global.scss";
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <BrowserRouter>
-      <GlobalProvider>{children}</GlobalProvider>
+      <GlobalProvider>
+        <LocaleProvider>{children}</LocaleProvider>
+      </GlobalProvider>
     </BrowserRouter>
   );
 }
