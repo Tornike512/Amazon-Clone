b7d1f0d6ea8a18962e5ac732540ba7ec5ac82310
Tornike512 <Dht5152@gmail.com>
2024-01-07 18:05:28 +0400

    Add: Language Change Modal Triangle

--- src/features/LanguageChange/LanguageChange.tsx
+++ src/features/LanguageChange/LanguageChange.tsx
@@ -1,17 +1,22 @@
+import triangle from "@src/assets/triangle.png";
+
 export function LanguageChange() {
   return (
-    <div className="language-change">
-      <span>
-        Change language <a href="#">Learn more</a>
-      </span>
-      <span>
-        <input type="checkbox" />
-        <span>English - EN</span>
-      </span>
-      <span>
-        <input type="checkbox" />
-        <span>Deutsch - DE</span>
-      </span>
-    </div>
+    <>
+      <img className="triangle" src={triangle} alt="White Triangle" />
+      <div className="language-change">
+        <span>
+          Change language <a href="#">Learn more</a>
+        </span>
+        <span>
+          <input type="checkbox" />
+          <span>English - EN</span>
+        </span>
+        <span>
+          <input type="checkbox" />
+          <span>Deutsch - DE</span>
+        </span>
+      </div>
+    </>
   );
 }
