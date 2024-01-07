384c720660ecf3e54634884d31b930e46439b1f7
Tornike512 <Dht5152@gmail.com>
2024-01-07 20:10:31 +0400

    Add: Language Change Panel

--- src/features/LanguageChange/LanguageChange.tsx
+++ src/features/LanguageChange/LanguageChange.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import triangle from "@src/assets/triangle.png";
 
 export function LanguageChange() {
+  const [enCheckBox, setEnCheckBox] = useState<boolean>(true);
+  const [deCheckBox, setDeCheckBox] = useState<boolean>(false);
+
   return (
     <>
       <img className="triangle" src={triangle} alt="White Triangle" />
@@ -8,13 +12,37 @@ export function LanguageChange() {
         <span className="language-change-text">
           Change language <a href="#">Learn more</a>
         </span>
-        <div>
-          <input type="checkbox" />
-          <span>English - EN</span>
+        <div className="en-language-checkbox">
+          <div
+            onClick={() => {
+              if (enCheckBox) {
+                setEnCheckBox(true);
+              } else {
+                setEnCheckBox(true);
+                setDeCheckBox(false);
+              }
+            }}
+            className="en-checkbox-border"
+          >
+            <div className={enCheckBox ? "en-checkbox" : ""}></div>
+          </div>
+          <span className="en-language">English - EN</span>
         </div>
-        <div>
-          <input type="checkbox" />
-          <span>Deutsch - DE</span>
+        <div className="de-language-checkbox">
+          <div
+            onClick={() => {
+              if (deCheckBox) {
+                setDeCheckBox(true);
+              } else {
+                setDeCheckBox(true);
+                setEnCheckBox(false);
+              }
+            }}
+            className="de-checkbox-border"
+          >
+            <div className={deCheckBox ? "de-checkbox" : ""}></div>
+          </div>
+          <span className="de-language">Deutsch - DE</span>
         </div>
       </div>
     </>
