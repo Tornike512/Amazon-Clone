8df53163ff037630439becb28d9e5b3df0a4449b
Tornike512 <Dht5152@gmail.com>
2024-01-25 16:46:20 +0400

    Add:Home Page Categories API

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,3 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface TCategories {
+  id: string;
+  created_at?: string;
+  updated_at: string;
+  name: string;
+}
+
 export function Home() {
-  return <div>home</div>;
+  const [categories, setCategories] = useState<TCategories[]>([]);
+
+  async function getCategories() {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/product-category"
+      );
+      setCategories(response.data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return (
+    <div>
+      {categories.map((category) => {
+        return <div key={category.id}>{category.name}</div>;
+      })}
+    </div>
+  );
 }
