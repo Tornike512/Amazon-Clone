3e20ca5e54fcf92ae38ef6c7ab3deb0ed4250635
Tornike512 <Dht5152@gmail.com>
2024-01-28 18:05:03 +0400

    Add: Keep Tokens In Local Storage

--- src/@types/RequestTypes.ts
+++ src/@types/RequestTypes.ts
@@ -2,3 +2,11 @@ export interface TAuthRequest {
   access_token: string;
   refresh_token: string;
 }
+
+export interface TUserRequest {
+  email: string;
+  id: string;
+  first_name: string;
+  last_name: string;
+  phone_number: null | string;
+}
--- src/providers/AuthProvider/AuthProvider.tsx
+++ src/providers/AuthProvider/AuthProvider.tsx
@@ -1,14 +1,25 @@
 import { PropsWithChildren, useState } from "react";
 import { AuthContext, TAuthorizationStatus_Enum } from "./AuthContext";
-import { TAuthRequest } from "@src/@types/RequestTypes";
+import { TAuthRequest, TUserRequest } from "@src/@types/RequestTypes";
+import { jwtDecode } from "jwt-decode";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "@src/config/LocalStorageKeys";
+import { setPrivateAccessToken } from "@src/utils/PrivateAxios";
 
 export function AuthProvider({ children }: PropsWithChildren) {
   const [authStatus, setAuthStatus] = useState<TAuthorizationStatus_Enum>(
     TAuthorizationStatus_Enum.UNAUTHORIZED
   );
 
+  const [userData, setUserData] = useState<TUserRequest>();
+  console.log(userData);
+
   function setAuthData(tokens: TAuthRequest) {
-    console.log(tokens);
+    const userData: TUserRequest = jwtDecode(tokens.access_token);
+    setUserData(userData);
+    localStorage.setItem(ACCESS_TOKEN, tokens.access_token);
+    localStorage.setItem(REFRESH_TOKEN, tokens.refresh_token);
+    setPrivateAccessToken(tokens.access_token);
+    setAuthStatus(TAuthorizationStatus_Enum.AUTHORIZED);
   }
 
   return (
