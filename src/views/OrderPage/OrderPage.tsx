1e4d280fd075ea1ffd1a95ff56e2d2b9fa2a973f
Tornike512 <Dht5152@gmail.com>
2024-02-15 20:32:25 +0400

    Add: Orders Page Html

--- src/views/OrderPage/OrderPage.tsx
+++ src/views/OrderPage/OrderPage.tsx
@@ -1,5 +1,52 @@
 import "@src/views/OrderPage/OrderPage.scss";
 
 export function OrderPage() {
-  return <div>knl.lk.</div>;
+  const orderInfos = [
+    "Orders",
+    "Buy Again",
+    "Not Yet Shipped",
+    "Digital Orders",
+    "Local Store Orders",
+    "Cancelled Orders",
+  ];
+
+  return (
+    <div className="orders">
+      <nav className="orders-nav">
+        <a href="#">Your Account</a>
+        <a href="#">Your Orders</a>
+      </nav>
+      <div className="search-orders">
+        <h1>Your Orders</h1>
+        <span>
+          <img src="" alt="Search Icon" />
+          <input type="text" placeholder="Search all orders" />
+        </span>
+        <button>Search Orders</button>
+      </div>
+      <ul className="order-infos">
+        {orderInfos.map((orderInfo) => {
+          return <li>{orderInfo}</li>;
+        })}
+      </ul>
+      <span className="divider"></span>
+      <div>
+        <label>
+          <span>0 Orders</span> placed in
+        </label>
+        <select name="orders-date" id="orders">
+          <option value="last-30-days">last 30 days</option>
+          <option value="past-3-months">past 3 months</option>
+          <option value="2024">2024</option>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+        </select>
+      </div>
+      <p className="past-orders">
+        You have not placed any orders in past 3 months.
+        <a href="#"> View orders in 2024</a>
+      </p>
+    </div>
+  );
 }
