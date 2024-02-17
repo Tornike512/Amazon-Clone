eb45b0a92270770322e5048f563ab775ea9cdb02
Tornike512 <Dht5152@gmail.com>
2024-02-17 22:44:48 +0400

    Add: PublicLayout OrdersPage

--- src/layouts/PublicLayout/PublicLayout.tsx
+++ src/layouts/PublicLayout/PublicLayout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "@src/components/Header";
+import OrderPage from "@src/views/OrderPage";
 
 export function PublicLayout() {
+  const currentUrl = window.location.href;
+
   return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
+    <>
+      <div>
+        <Header />
+        {currentUrl.includes("orders") ? <OrderPage /> : <Outlet />}
+      </div>
+    </>
   );
 }
