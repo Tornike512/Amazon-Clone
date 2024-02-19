32c0b321edeee0586032d216be77dfae9889d6a1
Tornike512 <Dht5152@gmail.com>
2024-02-20 01:32:15 +0400

    Add: Filter Product Style

--- src/views/ProductsPage/ProductsPage.tsx
+++ src/views/ProductsPage/ProductsPage.tsx
@@ -5,19 +5,21 @@ export function ProductsPage() {
     <div className="products-page">
       <div className="filter-products">
         <span>Department</span>
-        <span>Computers</span>
-        <a href="#">
-          Computer Accessories &<br /> Peripherals
-        </a>
-        <a href="#">Computer Components</a>
-        <a href="#">Computers & Tablets</a>
-        <a href="#">Data Storage</a>
-        <a href="#">Laptop Accessories</a>
-        <a href="#">Monitors</a>
-        <a href="#">Networking Products</a>
-        <a href="#">Servers</a>
-        <a href="#">Tablet Accessories</a>
-        <a href="#">Tablet Replacement Parts</a>
+        <span className="category">Computers</span>
+        <div className="choose-category">
+          <a href="#">
+            Computer Accessories &<br /> Peripherals
+          </a>
+          <a href="#">Computer Components</a>
+          <a href="#">Computers & Tablets</a>
+          <a href="#">Data Storage</a>
+          <a href="#">Laptop Accessories</a>
+          <a href="#">Monitors</a>
+          <a href="#">Networking Products</a>
+          <a href="#">Servers</a>
+          <a href="#">Tablet Accessories</a>
+          <a href="#">Tablet Replacement Parts</a>
+        </div>
       </div>
       <div className="products"></div>
     </div>
