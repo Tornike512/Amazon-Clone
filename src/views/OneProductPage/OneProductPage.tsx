340ca0ac3cb7656dd1ac70bc55036d8b266a78eb
Tornike512 <Dht5152@gmail.com>
2024-02-22 21:10:25 +0400

    Add: One Product Description

--- src/views/OneProductPage/OneProductPage.tsx
+++ src/views/OneProductPage/OneProductPage.tsx
@@ -91,42 +91,38 @@ export function OneProductPage() {
               Available at a lower price from other sellers that may not offer
               free Prime shipping.
             </p>
-            <h3>About this item</h3>
-            <p>{oneProduct?.description}</p>
+            <h3 className="about-this-item">About this item</h3>
+            <ul className="description">
+              <li>{oneProduct?.description}</li>
+            </ul>
           </div>
-          <p>
-            Note: Products with electrical plugs are designed for use in the US.
-            Outlets and voltage differ internationally and this product may
-            require an adapter or converter for use in your destination. Please
-            check compatibility before purchasing.
-          </p>
-        </div>
-        <div className="buy">
-          <h2>{oneProduct?.salePrice}</h2>
-          <p>Delivery order within 6 hrs 8 min</p>
-          <span>
-            <img src="" alt="" />
-            <a href="#">{deliverTo}</a>
-          </span>
-          <h2>In Stock</h2>
-          <select name="quantity" id="quantity">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-          <button>Add to Cart</button>
-          <button>Buy Now</button>
-          <span>
-            <span>Ships from</span>
-            <span>Amazon.com</span>
-          </span>
-          <span>
-            <span>sold by</span>
-            <span>Amazon.com</span>
-          </span>
-          <button>Add to List</button>
+          <div className="buy">
+            <h2>{oneProduct?.salePrice}</h2>
+            <p>Delivery order within 6 hrs 8 min</p>
+            <span>
+              <img src="" alt="" />
+              <a href="#">{deliverTo}</a>
+            </span>
+            <h2>In Stock</h2>
+            <select name="quantity" id="quantity">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <button>Add to Cart</button>
+            <button>Buy Now</button>
+            <span>
+              <span>Ships from</span>
+              <span>Amazon.com</span>
+            </span>
+            <span>
+              <span>sold by</span>
+              <span>Amazon.com</span>
+            </span>
+            <button>Add to List</button>
+          </div>
         </div>
       </div>
     </div>
