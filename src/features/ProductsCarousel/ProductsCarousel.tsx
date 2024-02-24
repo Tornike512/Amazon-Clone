8f7340243795100fcbb99272b0e654c2744f6151
Tornike512 <Dht5152@gmail.com>
2024-02-24 14:58:17 +0400

    Add: Products Carousel HTML

--- src/features/ProductsCarousel/ProductsCarousel.tsx
+++ src/features/ProductsCarousel/ProductsCarousel.tsx
@@ -1,3 +1,80 @@
-export function ProductsCarousel() {
-  return <div></div>;
+import { TGetProducts } from "@src/@types/RequestTypes";
+
+import rightArrow from "@src/assets/right-arrow.png";
+import leftArrow from "@src/assets/left-arrow.png";
+
+interface TProductsCarousel {
+  item: TGetProducts;
+}
+
+export function ProductsCarousel({ item }: TProductsCarousel) {
+  return (
+    <div className="carousel">
+      <div className="carousel-item">
+        <img src="" alt="Carousel Product Image" />
+        <p className="carousel-product-title"></p>
+        <span>
+          <img src="" alt="Carousel Product Rating" />
+          <span>72,274</span>
+        </span>
+        <h4>price</h4>
+      </div>
+      <div className="carousel-item">
+        <img src="" alt="Carousel Product Image" />
+        <p className="carousel-product-title"></p>
+        <span>
+          <img src="" alt="Carousel Product Rating" />
+          <span>72,274</span>
+        </span>
+        <h4>price</h4>
+      </div>
+      <div className="carousel-item">
+        <img src="" alt="Carousel Product Image" />
+        <p className="carousel-product-title"></p>
+        <span>
+          <img src="" alt="Carousel Product Rating" />
+          <span>72,274</span>
+        </span>
+        <h4>price</h4>
+      </div>
+      <div className="carousel-item">
+        <img src="" alt="Carousel Product Image" />
+        <p className="carousel-product-title"></p>
+        <span>
+          <img src="" alt="Carousel Product Rating" />
+          <span>72,274</span>
+        </span>
+        <h4>price</h4>
+      </div>
+      <div className="carousel-item">
+        <img src="" alt="Carousel Product Image" />
+        <p className="carousel-product-title"></p>
+        <span>
+          <img src="" alt="Carousel Product Rating" />
+          <span>72,274</span>
+        </span>
+        <h4>price</h4>
+      </div>
+      <div className="carousel-item">
+        <img src="" alt="Carousel Product Image" />
+        <p className="carousel-product-title"></p>
+        <span>
+          <img src="" alt="Carousel Product Rating" />
+          <span>72,274</span>
+        </span>
+        <h4>price</h4>
+      </div>
+      <div className="carousel-item">
+        <img src="" alt="Carousel Product Image" />
+        <p className="carousel-product-title"></p>
+        <span>
+          <img src="" alt="Carousel Product Rating" />
+          <span>72,274</span>
+        </span>
+        <h4>price</h4>
+      </div>
+      <img src={rightArrow} alt="Right Arrow" />
+      <img src={leftArrow} alt="Left Arrow" />
+    </div>
+  );
 }
