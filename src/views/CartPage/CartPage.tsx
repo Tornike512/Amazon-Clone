757c2f379b66461606cf77416a6217fdcd66fe2e
Tornike512 <Dht5152@gmail.com>
2024-02-28 00:19:43 +0400

    Add: Cart Page Subtotal

--- src/views/CartPage/CartPage.tsx
+++ src/views/CartPage/CartPage.tsx
@@ -37,59 +37,67 @@ export function CartPage() {
   }, []);
 
   return (
-    <div className="cart-page">
-      <div className="cart">
-        <div className="shopping-cart">
-          <h1>Shopping Cart</h1>
-          <span className="price-text">Price</span>
-          {cartProducts?.map((item) => {
-            return (
-              <div key={item.cartProduct.id} className="cart-product">
-                <span className="cart-product-image">
-                  <img src={item.cartProduct.image} alt="Product-image" />
-                </span>
-                <div className="cart-product-info">
-                  <p className="cart-product-title">{item.cartProduct.title}</p>
-                  <span className="in-stock-text">In Stock</span>
-                  <span className="cart-quantity">
-                    <select
-                      className="select-quantity"
-                      name="quantity"
-                      id="quantity"
-                    >
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                    </select>
-                    <span className="delete-cart-product">Delete</span>
-                    <span className="save-for-later">Save for later</span>
+    <div className="cart-page-split">
+      <div className="cart-page">
+        <div className="cart">
+          <div className="shopping-cart">
+            <h1>Shopping Cart</h1>
+            <span className="price-text">Price</span>
+            {cartProducts?.map((item) => {
+              return (
+                <div key={item.cartProduct.id} className="cart-product">
+                  <span className="cart-product-image">
+                    <img src={item.cartProduct.image} alt="Product-image" />
                   </span>
+                  <div className="cart-product-info">
+                    <p className="cart-product-title">
+                      {item.cartProduct.title}
+                    </p>
+                    <span className="in-stock-text">In Stock</span>
+                    <span className="cart-quantity">
+                      <select
+                        className="select-quantity"
+                        name="quantity"
+                        id="quantity"
+                      >
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                      </select>
+                      <span className="delete-cart-product">Delete</span>
+                      <span className="save-for-later">Save for later</span>
+                    </span>
+                  </div>
+                  <span className="cart-product-price">{`$${item.cartProduct.salePrice}.99`}</span>
                 </div>
-                <span className="cart-product-price">{`$${item.cartProduct.salePrice}.99`}</span>
-              </div>
-            );
-          })}
-          <span className="cart-product-subtotal">Subtotal 1 item $21.99</span>
+              );
+            })}
+            <span className="cart-product-subtotal">
+              Subtotal (1 item): <h5>$21.99</h5>
+            </span>
+          </div>
         </div>
-        <div className="subtotal">
-          <span className="subtotal-price">subtotal 1 item $21.99</span>
-          <button className="checkout">Proceed to checkout</button>
+        <div className="save-for-later">
+          <h2>Your items</h2>
+          <span>No items saved for later</span>
+          <div className="saved-for-later-product">
+            <img src="" alt="Saved For Later Image" />
+            <p className="saved-for-later-title"></p>
+            <span>$21.99</span>
+            <span>In Stock</span>
+            <button className="move-to-cart">Move to cart</button>
+            <span>Delete</span>
+            <span>Add to list</span>
+          </div>
         </div>
       </div>
-      <div className="save-for-later">
-        <h2>Your items</h2>
-        <span>No items saved for later</span>
-        <div className="saved-for-later-product">
-          <img src="" alt="Saved For Later Image" />
-          <p className="saved-for-later-title"></p>
-          <span>$21.99</span>
-          <span>In Stock</span>
-          <button className="move-to-cart">Move to cart</button>
-          <span>Delete</span>
-          <span>Add to list</span>
-        </div>
+      <div className="subtotal">
+        <span className="subtotal-price">
+          Subtotal (1 item): <h3>$21.99</h3>
+        </span>
+        <button className="checkout">Proceed to checkout</button>
       </div>
     </div>
   );
