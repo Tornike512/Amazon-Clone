e81239e81487687fa300f3bba8107ca5c00b8488
Tornike512 <Dht5152@gmail.com>
2024-03-05 21:30:54 +0400

    Add: Phone Number And Full Name Input Fields

--- src/components/AddAddressModal/AddAddressModal.tsx
+++ src/components/AddAddressModal/AddAddressModal.tsx
@@ -12,28 +12,34 @@ export function AddAddressModal() {
             <img src={blackCloseIcon} alt="Close Image" />
           </button>
         </div>
-        <h1 className="add-address-text">Add a new address</h1>
-        <form>
-          <label>Full name (First and Last name)</label>
-          <input className="full-name-input" type="text" />
-          <label>Phone number</label>
-          <input className="phone-number-input" type="text" />
-          <label>Address</label>
-          <input
-            className="address-input"
-            type="text"
-            placeholder="Street address or P.O. Box"
-          />
-          <span>
-            <label>City</label>
-            <label>ZIP Code</label>
-          </span>
-          <span>
-            <input className="city-input" type="text" />
-            <input className="zip-code-input" type="text" />
-          </span>
-          <button className="use-address-button">Use this address</button>
-        </form>
+        <div className="address-modal-info">
+          <h1 className="add-address-text">Add a new address</h1>
+          <form>
+            <div className="full-name-input">
+              <label>Full name (First and Last name)</label>
+              <input type="text" />
+            </div>
+            <div className="phone-number-input">
+              <label>Phone number</label>
+              <input type="text" />
+            </div>
+            <label>Address</label>
+            <input
+              className="address-input"
+              type="text"
+              placeholder="Street address or P.O. Box"
+            />
+            <span>
+              <label>City</label>
+              <label>ZIP Code</label>
+            </span>
+            <span>
+              <input className="city-input" type="text" />
+              <input className="zip-code-input" type="text" />
+            </span>
+            <button className="use-address-button">Use this address</button>
+          </form>
+        </div>
       </div>
     </div>
   );
