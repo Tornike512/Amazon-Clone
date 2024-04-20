dc8ead5f05eb2a8953d0f3aab8f87608801903c2
Tornike512 <Dht5152@gmail.com>
2024-04-20 21:39:45 +0400

    Add: Home Page Carousel Buttons Spacing

--- src/components/HomeCarousel/HomeCarousel.tsx
+++ src/components/HomeCarousel/HomeCarousel.tsx
@@ -3,10 +3,14 @@ import { useRef } from "react";
 
 import useGetProducts from "@src/hooks/useGetProducts";
 
+import rightArrow from "@src/assets/right-arrow.png";
+import leftArrow from "@src/assets/left-arrow.png";
+
 import "./HomeCarousel.scss";
 
 export function HomeCarousel({ category }: { category: string }) {
-  const reft = useRef();
+  const ref = useRef();
+  console.log(ref);
 
   const onChange = (currentSlide: number) => {
     console.log(currentSlide, "hello");
@@ -15,37 +19,43 @@ export function HomeCarousel({ category }: { category: string }) {
   const { products } = useGetProducts({ category });
 
   return (
-    <Carousel afterChange={onChange}>
-      <div>
-        <h3 className="content-style">
-          {products?.slice(0, 5).map((product) => {
-            return (
-              <>
-                <div key={product.id} className="content-item">
-                  <img src={product.image} alt="Product Image" />
-                </div>
-              </>
-            );
-          })}
-        </h3>
-        <Button className="switch-button">prev</Button>
-        <Button>next</Button>
-      </div>
-      <div>
-        <h3 className="content-style">
-          {products?.slice(5, 11).map((product) => {
-            console.log(products);
+    <div>
+      <Carousel ref={ref} afterChange={onChange}>
+        <div>
+          <h3 className="content-style">
+            {products?.slice(0, 5).map((product) => {
+              return (
+                <>
+                  <div key={product.id} className="content-item">
+                    <img src={product.image} alt="Product Image" />
+                  </div>
+                </>
+              );
+            })}
+          </h3>
+          <Button onClick={() => ref.current.prev()} className="left-switch">
+            <img src={leftArrow} alt="Left Arrow" />
+          </Button>
+          <Button onClick={() => ref.current.next()} className="right-switch">
+            <img src={rightArrow} alt="Right Arrow" />
+          </Button>
+        </div>
+        <div>
+          <h3 className="content-style">
+            {products?.slice(5, 11).map((product) => {
+              console.log(products);
 
-            return (
-              <>
-                <div key={product.id} className="content-item">
-                  <img src={product.image} alt="Product Image" />
-                </div>
-              </>
-            );
-          })}
-        </h3>
-      </div>
-    </Carousel>
+              return (
+                <>
+                  <div key={product.id} className="content-item">
+                    <img src={product.image} alt="Product Image" />
+                  </div>
+                </>
+              );
+            })}
+          </h3>
+        </div>
+      </Carousel>
+    </div>
   );
 }
