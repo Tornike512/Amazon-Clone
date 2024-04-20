9f2e9a10e5de08f561e69840bb84d77c301a1ebf
Tornike512 <Dht5152@gmail.com>
2024-04-20 17:58:48 +0400

    Add: Home Page Carousel Sliding To Different Products

--- src/components/HomeCarousel/HomeCarousel.tsx
+++ src/components/HomeCarousel/HomeCarousel.tsx
@@ -6,18 +6,16 @@ import "./HomeCarousel.scss";
 
 export function HomeCarousel({ category }: { category: string }) {
   const onChange = (currentSlide: number) => {
-    console.log(currentSlide);
+    console.log(currentSlide, "hello");
   };
 
   const { products } = useGetProducts({ category });
-
-  console.log(products);
 
   return (
     <Carousel afterChange={onChange}>
       <div>
         <h3 className="content-style">
-          {products?.map((product) => {
+          {products?.slice(0, 5).map((product) => {
             return (
               <>
                 <div key={product.id} className="content-item">
@@ -30,20 +28,9 @@ export function HomeCarousel({ category }: { category: string }) {
       </div>
       <div>
         <h3 className="content-style">
-          {products?.map((product) => {
-            return (
-              <>
-                <div key={product.id} className="content-item">
-                  <img src={product.image} alt="Product Image" />
-                </div>
-              </>
-            );
-          })}
-        </h3>
-      </div>
-      <div>
-        <h3 className="content-style">
-          {products?.map((product) => {
+          {products?.slice(5, 11).map((product) => {
+            console.log(products);
+
             return (
               <>
                 <div key={product.id} className="content-item">
