90f29a91544a369853a304f92e93c6ea4aea3286
Tornike512 <Dht5152@gmail.com>
2024-04-21 14:18:30 +0400

    Add: Footer HTML

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -71,6 +71,123 @@ export function Footer() {
           </ul>
         </div>
       </div>
+      <div className="footer-secondary-grid-container">
+        <ul className="footer-secondary-grid">
+          <li className="footer-secondary-grid-item">
+            <h6>Amazon Music</h6>
+            <p>Stream millions of songs</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Amazon Ads</h6>
+            <p>Reach customers wherever they spend their time</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>6pm</h6>
+            <p>Score deals on fashion brands</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>AbeBooks</h6>
+            <p>Books, art & collectibles</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>ACX</h6>
+            <p>Audiobook Publishing Made Easy</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Sell on Amazon</h6>
+            <p>Start a Selling Account</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Amazon Business</h6>
+            <p>Everything For Your Business</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>AmazonGlobal</h6>
+            <p>Ship Orders Internationally</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Home Services</h6>
+            <p>Experienced Pros Happiness Guarantee</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Amazon Web Services</h6>
+            <p>Scalable Cloud Computing Services</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Audible</h6>
+            <p>Listen to Books & Original Audio Performances </p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Box Office Mojo</h6>
+            <p>Find Movie Box Office Data</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Goodreads</h6>
+            <p>Book reviews & recommendations</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>IMDb</h6>
+            <p>Movies, TV & Celebrities</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>IMDbPro</h6>
+            <p>Get Info Entertainment Professionals Need </p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Kindle Direct Publishing</h6>
+            <p>Indie Digital & Print Publishing Made Easy </p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Prime Video Direct</h6>
+            <p>Video Distribution Made Easy </p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Shopbop</h6>
+            <p>Designer Fashion Brands</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Woot!</h6>
+            <p>Deals and Shenanigans</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Zappos</h6>
+            <p>Shoes & Clothing</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Ring</h6>
+            <p>Smart Home Security Systems</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>eero WiFi</h6>
+            <p>Stream 4K Video in Every Room</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Blink</h6>
+            <p>Smart Security for Every Home</p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Neighbors App</h6>
+            <p>Real-Time Crime & Safety Alerts </p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>Amazon Subscription Boxes</h6>
+            <p>Top subscription boxes - right to your door </p>
+          </li>
+          <li className="footer-secondary-grid-item">
+            <h6>PillPack</h6>
+            <p>Pharmacy Simplified</p>
+          </li>
+        </ul>
+      </div>
+      <ul className="footer-conditons-of-use">
+        <li>Conditions of Use</li>
+        <li>Privacy Notice</li>
+        <li>Consumer Health Data Privacy Disclosure</li>
+        <li>Your Ads Privacy Choices</li>
+      </ul>
+      <ul className="made-by">
+        <li>Made By @Tornike Tsagareishvili</li>
+      </ul>
     </footer>
   );
 }
