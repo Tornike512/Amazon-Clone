9e939e5355bcdd7186da8b5b765446afa42fed5d
Tornike Tsagareishvili <139069717>
2024-05-21 22:38:07 +0400

    Add: Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@src/App.tsx";
 import { Providers } from "@src/providers/Providers.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@src/sass/global.scss";
 
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Providers>
       <App />
+      <Analytics />
     </Providers>
   </React.StrictMode>
 );
